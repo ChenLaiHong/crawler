bd0f23a5637d017fe3148380031a6958c5764c4a
ChenLaiHong <1185630400@qq.com>
2018-11-29 19:11:29 +0800

    爬取酷狗Top500

--- simple/kugou.py
+++ simple/kugou.py
@@ -7,7 +7,7 @@ mydb = mysql.connector.connect(
     host="localhost",       # 数据库主机地址
     user="root",    # 数据库用户名
     passwd="123456",   # 数据库密码
-    database="test"
+    database="test"  # 先自己手动创建数据库
 )
 mycursor = mydb.cursor()
 
